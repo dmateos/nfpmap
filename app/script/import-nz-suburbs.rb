2b6cce6598457c89f1cf92119d64ef56c9e77d38
Daniel Mateos <daniel@mateos.cc>
2014-12-15 21:02:44 +1030

    nz suburbs import

---
+++ app/script/import-nz-suburbs.rb
@@ -0,0 +1,15 @@
+require 'csv'
+
+csv_data = File.read('app/script/nz-suburbs.csv')
+csv = CSV.parse(csv_data, headers: false)
+
+csv.each do |c|
+  suburb = Suburb.new
+  suburb.name = c[1]
+  suburb.postcode = c[0]
+  suburb.state = "n/a"
+  suburb.lat = c[2]
+  suburb.long = c[3]
+
+  suburb.save
+end
